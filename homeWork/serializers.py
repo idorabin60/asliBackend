e299cfcde1ae58a39cc69aaf1d45ed5ce7c7db5e
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-05-29 01:51:21 +0300

    staging

--- homeWork/serializers.py
+++ homeWork/serializers.py
@@ -1,15 +1,55 @@
+import json
 from rest_framework import serializers
-from .models import Homework
+from .models import (
+    Homework,
+    VocabularyMatch,
+    FillInBlank,
+    GrammaticalPhenomenon
+)
+
+
+class VocabularyMatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VocabularyMatch
+        fields = ['id', 'arabic_word', 'hebrew_word', 'homework']
+
+
+class FillInBlankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FillInBlank
+        fields = ['id', 'sentence', 'options',
+                  'correct_option', 'homework', 'hebrew_sentence']
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        try:
+            # convert JSON string to list
+            rep['options'] = json.loads(rep['options'])
+        except json.JSONDecodeError:
+            rep['options'] = []
+        return rep
+
+
+class GrammaticalPhenomenonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GrammaticalPhenomenon
+        fields = ['id', 'text', 'homework']
 
 
 class HomeworkSerializer(serializers.ModelSerializer):
-    # Ensures the user field is properly serialized
+    vocab_matches = VocabularyMatchSerializer(many=True, read_only=True)
+    fill_in_blanks = FillInBlankSerializer(many=True, read_only=True)
+    grammatical_phenomenon = GrammaticalPhenomenonSerializer(read_only=True)
     user = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Homework
         fields = [
-            'id', 'user', 'created_at', 'file_id',
-            'summary', 'new_vocabulary', 'grammatical_phenomenon', 'hw'
+            'id',
+            'user',
+            'due_date',
+            'vocab_matches',
+            'fill_in_blanks',
+            'grammatical_phenomenon',
+
         ]
-        read_only_fields = ['id', 'created_at', 'file_id']
