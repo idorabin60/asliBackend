cd8208051e315df77eed46b25161a47ee7c83baa
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-05-29 11:02:59 +0300

    staging

--- asliBackend/urls.py
+++ asliBackend/urls.py
@@ -1,30 +1,15 @@
-"""
-URL configuration for asliBackend project.
+# from django.contrib import admin
+# from rest_framework.authtoken import views as token_views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, re_path
 from users import views as user_views
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path('login', user_views.login),
-    re_path('test_token', user_views.test_token),
-    re_path('signup', user_views.signup),
+    # path('admin/', admin.site.urls),
+    # path('api-token-auth/', token_views.obtain_auth_token),
+
+    re_path('login',       user_views.login),
+    re_path('test_token',  user_views.test_token),
+    re_path('signup',      user_views.signup),
     re_path('user_homeworks', user_views.user_homeworks),
-
-
 ]
--- homeWork/admin.py
+++ homeWork/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
-from .models import Homework  # Import your models
+# from django.contrib import admin
+# from .models import Homework  # Import your models
 
-admin.site.register(Homework)  # Register the model
+# admin.site.register(Homework)  # Register the model
