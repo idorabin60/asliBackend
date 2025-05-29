cd09c6356b970f0f509f708be5fcde2835246b8d
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-05-29 11:08:47 +0300

    bla

--- asliBackend/urls.py
+++ asliBackend/urls.py
@@ -1,15 +1,52 @@
-# from django.contrib import admin
-# from rest_framework.authtoken import views as token_views
+"""
+URL configuration for asliBackend project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path, re_path
 from users import views as user_views
+from homeWork import views as hw_views
+
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('api-token-auth/', token_views.obtain_auth_token),
-
-    re_path('login',       user_views.login),
-    re_path('test_token',  user_views.test_token),
-    re_path('signup',      user_views.signup),
+    path('admin/', admin.site.urls),
+    re_path('login', user_views.login),
+    re_path('test_token', user_views.test_token),
+    re_path('signup', user_views.signup),
     re_path('user_homeworks', user_views.user_homeworks),
+    re_path('all', hw_views.get_all_hw),
+    path("homeworks/<int:homework_id>/", user_views.get_homework_by_id),
+    path("get_teachers", user_views.get_all_teachers),
+    path(
+        'students_of_teacher/<int:teacher_id>/',
+        user_views.students_of_teacher,
+        name='students-of-teacher'
+    ),
+    path(
+        'students/<int:student_id>/homeworks/',
+        hw_views.student_homeworks,
+        name='student-homeworks'
+    ),
+    path(
+        'students/<int:student_id>/homeworks/<int:homework_id>/',
+        hw_views.student_homework_detail,
+        name='student-homework-detail'
+    ),
+    path(
+        'users/<int:user_id>/',
+        user_views.get_user_by_id,
+        name='get-user-by-id'
+    ),
 ]
