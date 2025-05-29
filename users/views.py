4f31226f06d6ffd84e6c3b6b9c3b8b2b41bff76a
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-05-29 11:12:55 +0300

    k

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
@@ -10,65 +9,7 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from homeWork.serializers import HomeworkSerializer  # Import Homework serializer
 from homeWork.models import Homework  # Import Homework model
-
-
-@api_view(['POST'])
-def login(request):
-    user = get_object_or_404(User, email=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found"}, status=status.HTTP_404_NOT_FOUND)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
-
-
-@api_view(['POST'])
-def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        validated_data = serializer.validated_data
-        email = validated_data.get('email')
-        if User.objects.filter(email=email).exists():
-            return Response(
-                {"error": "User with this email already exists"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        password = validated_data.pop('password', None)
-
-        user = User(**validated_data)
-
-        if password:
-            user.set_password(password)
-
-        user.save()
-
-        # Generate authentication token
-        token, created = Token.objects.get_or_create(user=user)
-
-        # Serialize the user AFTER saving to include the correct data
-        user_serializer = UserSerializer(user)
-        print(user_serializer)
-
-        return Response({"token": token.key, "user": user_serializer.data}, status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-def test_token(request):
-    return Response({})
-
-
-@api_view(['GET'])
-@authentication_classes([TokenAuthentication, SessionAuthentication])
-@permission_classes([IsAuthenticated])
-def user_homeworks(request):
-    """ Get all homeworks for the logged-in user """
-    homeworks = Homework.objects.filter(user=request.user)
-    serializer = HomeworkSerializer(homeworks, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
